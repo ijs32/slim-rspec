f34f6c9ea26ee18caba70ac0d04be06f412dbe41
Ian S <iansilber32@gmail.com>
2022-03-19 16:55:13 -0400

    struggling I am bad at this

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Product.create!([
+  {name: "product1", price: 200.99}, 
+  {name: "product2", price: 400.99},
+  {name: "product3", price: 800.99}, 
+  {name: "product4", price: 1600.99},
+  {name: "product5", price: 3200.99}, 
+  {name: "product6", price: 6400.99}
+])
